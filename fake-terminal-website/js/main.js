a44cd97f26ef23a58a94dff3b5bca49c6206608e
wimel <angel.contreras.mejias@gmail.com>
2018-12-19 21:23:52 +0100

    modify background and default options

--- fake-terminal-website/js/main.js
+++ fake-terminal-website/js/main.js
@@ -67,12 +67,14 @@ var files = (function () {
         }
     };
     Singleton.defaultOptions = {
-        "about me.txt": "This website was made using only pure JavaScript with no extra libraries.\nI made it dynamic so anyone can use it, just download it from GitHub and change the config text according to your needs.\nIf you manage to find any bugs or security issues feel free to email me: luisbraganca@protonmail.com",
-        "getting_started.txt": "First, go to js/main.js and replace all the text on both singleton vars.\n- configs: All the text used on the website.\n- files: All the fake files used on the website. These files are also used to be listed on the sidenav.\nAlso please notice if a file content is a raw URL, when clicked/concatenated it will be opened on a new tab.\nDon't forget also to:\n- Change the page title on the index.html file\n- Change the website color on the css/main.css\n- Change the images located at the img folder. The suggested sizes are 150x150 for the avatar and 32x32/16x16 for the favicon.",
-        "contact.txt": "wimeleth85@gmail.com",
+        "About me.txt": "This website was made using only pure JavaScript with no extra libraries.\nI made it dynamic so anyone can use it, just download it from GitHub and change the config text according to your needs.\nIf you manage to find any bugs or security issues feel free to email me: luisbraganca@protonmail.com",
+        "Projects.txt": "First, go to js/main.js and replace all the text on both singleton vars.\n- configs: All the text used on the website.\n- files: All the fake files used on the website. These files are also used to be listed on the sidenav.\nAlso please notice if a file content is a raw URL, when clicked/concatenated it will be opened on a new tab.\nDon't forget also to:\n- Change the page title on the index.html file\n- Change the website color on the css/main.css\n- Change the images located at the img folder. The suggested sizes are 150x150 for the avatar and 32x32/16x16 for the favicon.",
+        "Contact.txt": "wimeleth85@gmail.com",
         "Github.txt": "https://github.com/wimel",
         "Medium.txt": "https://medium.com/@wimel",
-        "Keybase.txt": "https://keybase.io/wimel"
+        "Twitter.txt": "https://twitter.com/wimel85",
+        "Keybase.txt": "https://keybase.io/wimel",
+        "Linkedin.txt": "https://www.linkedin.com/in/angelsvq"
     };
     return {
         getInstance: function (options) {
